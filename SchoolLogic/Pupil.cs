b2303c17b8ad8161b27ab0b7d79efd4bb2f824b7
Markus Meyer <Markus.Meyer@plan-b-gmbh.com>
2020-02-10 16:01:32 +0100

    Add pupil

---
+++ SchoolLogic/Pupil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLogic
+{
+    public class Pupil
+    {
+        /// <summary>
+        /// Gets or sets the school subjects.
+        /// </summary>
+        /// <value>
+        /// The school subjects.
+        /// </value>
+        public List<SchoolSubject> SchoolSubjects { get; set; }
+
+        /// <summary>
+        /// Calculates the average.
+        /// </summary>
+        /// <returns></returns>
+        public double CalculateAverage()
+        {
+            double result = 0;
+            return result;
+        }
+    }
+}
