3264526f87102f84b9ef1439ce7568526c97470f
Markus Meyer <Markus.Meyer@plan-b-gmbh.com>
2020-02-13 06:20:17 +0100

    Add Schoolclass

---
+++ SchoolLogic/SchoolClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLogic
+{
+    public class SchoolClass
+    {
+        public List<Pupil> Pupils { get; set; }
+
+        public double CalculateAverage()
+        {
+            double result = 0;
+            return result;
+        }
+
+    }
+}
