d86a9c788848f1e150244f6f7dc7d0715d193fe3
Markus Meyer <Markus.Meyer@plan-b-gmbh.com>
2020-04-24 10:31:42 +0200

    Add function

---
+++ SchoolFunction/Function1.cs
@@ -0,0 +1,59 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace SchoolFunction
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("Average")]
+        public static async Task<IActionResult> Average(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            double number1 = double.NaN;
+            if (req.Query.ContainsKey("number1"))
+            {
+                double.TryParse(req.Query["number1"], out number1);
+            }
+
+            double number2 = double.NaN;
+            if (req.Query.ContainsKey("number2"))
+            {
+                double.TryParse(req.Query["number2"], out number2);
+            }
+
+            string responseMessage = ((number1 + number2)/2).ToString();
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
